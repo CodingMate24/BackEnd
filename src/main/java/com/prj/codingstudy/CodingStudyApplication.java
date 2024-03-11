d0fed0b41c0345863f1d349f8a539b987e872a5a
msi <shlee8689@gmail.com>
2024-03-11 16:43:34 +0900

    [추가] 최초작성.

---
+++ src/main/java/com/prj/codingstudy/CodingStudyApplication.java
@@ -0,0 +1,13 @@
+package com.prj.codingstudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodingStudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CodingStudyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/prj/codingstudy/CodingStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prj.codingstudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodingStudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
